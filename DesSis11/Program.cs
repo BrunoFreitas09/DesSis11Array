28f5beebf36567f9a59b5ae8cc5708edf7165bc6
Usuario <Usuario@CPD02-M04>
2024-04-30 10:42:07 -0300

    Adicionar arquivos de projeto.

---
+++ DesSis11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesSis11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = { "JOÃO", "PEDRO", "MARIA" };
+            Console.WriteLine("Digite um numero de 0 a 2: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("O nome selecionado foi: " + nome[i]);
+               
+
+            Console.ReadKey(); 
+        }
+    }
+}
